5850bf3fc0ce3e0e74985ac2a89e40dfd1a89e95
BryceStampfl <BryceStampfl@gmail.com>
2022-04-17 21:04:45 -0700

    committing settings files

---
+++ client/babel.config.js
@@ -0,0 +1,29 @@
+module.exports = function (api) {
+  api.cache(true);
+  return {
+    presets: ['babel-preset-expo'],
+    env: {
+      production: {
+        plugins: ['react-native-paper/babel'],
+      },
+    },
+    plugins: [
+      [
+        'module-resolver',
+        {
+          extensions: [
+            '.js',
+            '.jsx',
+            '.ts',
+            '.tsx',
+            '.android.js',
+            '.android.tsx',
+            '.ios.js',
+            '.ios.tsx'
+          ],
+          root: ['.']
+        }
+      ]
+    ]
+  };
+};
