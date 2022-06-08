40d6e9d7e9c3601d61700f4ea7fad80cf50f7a9e
BryceStampfl <BryceStampfl@gmail.com>
2022-06-07 22:37:14 -0700

    updated app.tsx config

--- client/App.tsx
+++ client/App.tsx
@@ -4,6 +4,9 @@ import { ApolloProvider } from "@apollo/client";
 import Navigation from 'components/Navigation';
 import { apolloClient } from 'utils/DatabaseConnection';
 
+// Starting point of application
+// Apollo Provider Context is so we can use Apollo/Graphql for queries and mutations
+// Navigation Component is for bottom tabs
 export default function App() {
   return (
     <ApolloProvider client={apolloClient}>
