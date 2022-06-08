0cfbf9cd8f3d1afbcbf317e0529ddf63c49369ce
BryceStampfl <BryceStampfl@gmail.com>
2022-06-07 18:29:38 -0700

    Cleanred up TaskScreen imports and spacing

--- client/components/TaskScreen/index.tsx
+++ client/components/TaskScreen/index.tsx
@@ -1,12 +1,11 @@
 import React from 'react';
-import { Text, StyleSheet } from 'react-native';
-import SearchBar from './SearchBar/index';
-import TaskList from './TaskList/index';
-import TaskFilters from './TaskFilters/index'
-import { SafeAreaView } from 'react-native-safe-area-context';
-import AddTask from './AddTask';
-import Header from 'components/shared/Header';
+
 import ScreenWrapper from 'components/shared/ScreenWrapper';
+import Header from 'components/shared/Header';
+import SearchBar from './SearchBar/index';
+import AddTask from './AddTask';
+import TaskFilters from './TaskFilters/index'
+import TaskList from './TaskList/index';
 
 
 const TaskScreen = () => {
@@ -21,6 +20,7 @@ const TaskScreen = () => {
     const onSearchText = (text) => {
         setSearchText(text)
     }
+
     return (
         <ScreenWrapper>
             <Header title={"Missions"} />
@@ -31,18 +31,4 @@ const TaskScreen = () => {
         </ScreenWrapper>
     )
 }
-
 export default TaskScreen;
-
-
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        alignItems: 'center',
-    },
-    header: {
-        fontSize: 40,
-        color: '#FFFFFF',
-        alignSelf: 'center',
-    },
-});
